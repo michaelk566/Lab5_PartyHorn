43f03c53651cbc7240a67fb63847c5b9bad452fb
Michael Khanzadeh <68492504+michaelk566@users.noreply.github.com>
2021-02-06 02:56:14 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -47,7 +47,7 @@ num.onchange = function() {
 
 slider.onchange = function() {
     num.value = slider.value;
-  
+    audio.play();
        if(slider.value >= 67){
       document.getElementById('volume-image').src = "./assets/media/icons/volume-level-3.svg";
     }
