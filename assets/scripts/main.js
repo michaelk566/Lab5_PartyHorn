69fd3c98cd43bfd99448fee8bf486ab05d61fb7f
Michael Khanzadeh <68492504+michaelk566@users.noreply.github.com>
2021-02-05 19:58:18 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,6 @@
 // main.js
 
-const paragraphs = document.querySelectorAll("p");
+const paragraphs = document.querySelectorAll("h1");
 // paragraphs[0] is the first <p> element
 // paragraphs[1] is the second <p> element, etc.
 alert(paragraphs[0].nodeName);
