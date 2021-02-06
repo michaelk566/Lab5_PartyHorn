8c909de510b52724e2673dd93260c4ac3a0c609d
Michael Khanzadeh <68492504+michaelk566@users.noreply.github.com>
2021-02-05 19:57:46 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,6 @@
 // main.js
 
-var num = document.getElementById("volume-number");
-num.setAttribute(15)
-
+const paragraphs = document.querySelectorAll("p");
+// paragraphs[0] is the first <p> element
+// paragraphs[1] is the second <p> element, etc.
+alert(paragraphs[0].nodeName);
