39a5cd28ef5f2209d843cf0edc4c737e85dad4bf
Michael Khanzadeh <68492504+michaelk566@users.noreply.github.com>
2021-02-05 20:17:31 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,8 +3,9 @@
 document.getElementById("honk-btn-container").addEventListener("click", playHorn);
 
 function playHorn(){
+  document.getElementById("volume-slider").value = "75";
  alert("worked");
  document.getElementById("volume-slider").value = "75";
-  document.getElementById("horn-sound").addEventListener('play');
+ // document.getElementById("horn-sound").addEventListener('play');
  
 }
