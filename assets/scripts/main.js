9022a3238b312b31e89512ac0eae6504218ca0ed
Michael Khanzadeh <68492504+michaelk566@users.noreply.github.com>
2021-02-05 23:53:58 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -12,7 +12,7 @@ function playHorn(){
  
 }
 */
-document.getElementById('volume-image').src = "./assets/media/icons/volume-level-1.svg";
+
 var num = document.getElementById('volume-number');
 
 var slider = document.getElementById('volume-slider');
@@ -20,22 +20,22 @@ var slider = document.getElementById('volume-slider');
 
 num.onchange = function() {
     slider.value = num.value;
-  /*
+  
     if(slider.value>=67){
-      
+      document.getElementById('volume-image').src = "./assets/media/icons/volume-leve3-1.svg";
     }
     else if(slider.value<=66 && slider.value>=34){
-      
+      document.getElementById('volume-image').src = "./assets/media/icons/volume-level-2.svg";
       
     }
     else if(slider.value >0){
-      
+      document.getElementById('volume-image').src = "./assets/media/icons/volume-level-1.svg";
     }
   
     else{
-      
+      document.getElementById('volume-image').src = "./assets/media/icons/volume-level-0.svg";
     }
-    */
+    
   
   
 }
